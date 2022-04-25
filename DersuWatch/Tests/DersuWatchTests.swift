8c4f74c8d1b765739d49c91885c7511b3cca5197
Danila Goriachkin <dndv279@gmail.com>
2022-04-25 11:05:11 +0300

    Structure is added.

--- DersuWatch/Tests/DersuWatchTests.swift
+++ DersuWatch/Tests/DersuWatchTests.swift
@@ -1,14 +1,13 @@
 //
-//  DersuTests.swift
-//  DersuTests
+//  DersuWatchTests.swift
+//  DersuWatchTests
 //
-//  Created by Danila on 24.04.2022.
+//  Created by Danila on 25.04.2022.
 //
 
 import XCTest
-@testable import Dersu
 
-class DersuTests: XCTestCase {
+class DersuWatchTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -23,13 +22,13 @@ class DersuTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete.
-        // Check the results with assertions afterwards.
+        // Mark your test async to allow awaiting for asynchronous
+        // code to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
--- Shared/Tests/DersuSharedTests.swift
+++ Shared/Tests/DersuSharedTests.swift
@@ -1,14 +1,13 @@
 //
-//  Dersu_WatchKit_AppTests.swift
-//  Dersu WatchKit AppTests
+//  DersuSharedTests.swift
+//  DersuSharedTests
 //
-//  Created by Danila on 24.04.2022.
+//  Created by Danila on 25.04.2022.
 //
 
 import XCTest
-@testable import Dersu_WatchKit_Extension
 
-class DersuWatchKitAppTests: XCTestCase {
+class DersuSharedTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -29,7 +28,7 @@ class DersuWatchKitAppTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
